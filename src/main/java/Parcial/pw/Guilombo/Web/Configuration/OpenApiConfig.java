c9141d6cb6af025769bdb1e43e126c1f83e340e6
Frank_G <guilombofrank07@gmail.com>
2024-10-29 10:38:29 -0500

    Sistema completo(antes de documentacion)

---
+++ src/main/java/Parcial/pw/Guilombo/Web/Configuration/OpenApiConfig.java
@@ -0,0 +1,39 @@
+package Parcial.pw.Guilombo.Web.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sistema de Gestión de Asignaturas API")
+                        .version("1.0")
+                        .description("API para la gestión de asignaturas y usuarios del sistema académico")
+                        .contact(new Contact()
+                                .name("Guilombo")
+                                .email("contact@example.com")
+                                .url("https://www.example.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
